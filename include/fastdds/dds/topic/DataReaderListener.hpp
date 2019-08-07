71a3636bf7328a1430068ee7d2bf63391cd7a8b2
LauraMartin <laura@eprosima.com>
2019-08-07 10:22:15 +0200

    Apply changes SubscriptionMatchedStatus dds

--- include/fastdds/dds/topic/DataReaderListener.hpp
+++ include/fastdds/dds/topic/DataReaderListener.hpp
@@ -26,6 +26,7 @@
 #include <fastrtps/qos/SampleRejectedStatus.hpp>
 #include <fastrtps/qos/IncompatibleQosStatus.hpp>
 #include <fastdds/rtps/common/MatchingInfo.h>
+#include <fastdds/dds/core/status/SubscriptionMatchedStatus.h>
 
 namespace eprosima {
 namespace fastdds {
@@ -62,7 +63,7 @@ class RTPS_DllAPI DataReaderListener
          */
         virtual void on_subscription_matched(
                 DataReader* reader,
-                fastrtps::rtps::MatchingInfo& info)
+                fastdds::dds::SubscriptionMatchedStatus& info)
         {
             (void)reader;
             (void)info;
